483cfbc41394775ea96a6db54eef6fbc9e2e7aa0
Łukasz Sobczak <lukasz.k.sobczak@gmail.com>
2023-01-03 23:00:45 +0100

    task: add endpoint for closing reports

---
+++ backend/AbandonedMiracle.Api/Commands/Reports/CloseReport.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using AbandonedMiracle.Api.DataAccess;
+using AbandonedMiracle.Api.Entities.Reports;
+using AbandonedMiracle.Api.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbandonedMiracle.Api.Commands.Reports;
+
+public class CloseReport
+{
+    public class Command : IRequest<Unit>
+    {
+        public Guid ReportId { get; set; }
+    }
+    
+    public class Handler : IRequestHandler<Command, Unit>
+    {
+        private readonly AmDbContext _context;
+
+        public Handler(AmDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var report = await _context.Reports.FirstOrDefaultAsync(x => x.Id == request.ReportId);
+
+            if (report is null)
+            {
+                throw new RestException(HttpStatusCode.NotFound, "Report not found");
+            }
+            
+            if(report.Status == ReportStatus.Closed)
+            {
+                throw new RestException(HttpStatusCode.BadRequest, "Report is already closed");
+            }
+            
+            report.Status = ReportStatus.Closed;
+            
+            var success = await _context.SaveChangesAsync() > 0;
+            
+            if (success) return Unit.Value;
+            
+            throw new Exception("Problem saving changes");
+        }
+    }
+}
---
+++ backend/AbandonedMiracle.Api/Controllers/ServiceWorkerController.cs
@@ -0,0 +1,21 @@
+﻿using AbandonedMiracle.Api.Commands.Reports;
+using AbandonedMiracle.Api.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbandonedMiracle.Api.Controllers;
+
+public class ServiceWorkerController : AmController
+{
+    public ServiceWorkerController(IMediator mediator) : base(mediator)
+    {
+    }
+    
+    [HttpPost, Authorize(Roles = AmRole.ServiceWorker)]
+    public async Task<IActionResult> CloseReport([FromBody] CloseReport.Command command)
+    {
+        return await HandleAsync(command);
+    }
+    
+}
